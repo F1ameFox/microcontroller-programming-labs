5a40e28dfc27675a14f4869c17386e779ec04be7
F1ameFox <106865214+F1ameFox@users.noreply.github.com>
2024-02-14 21:13:02 +0700

    Add files via upload

---
+++ l3.ino
@@ -0,0 +1,70 @@
+#define DEBUG 0
+
+int pin[]={2,3,4,5};
+//bool flag = 1;
+volatile bool a0;
+volatile bool b0;
+volatile bool c0;
+int counter = 0;
+//bool binar[4];
+
+void setup() {
+  pinMode(A1,2);//DDRC &= ~(1 << PC1); // INPUT
+  pinMode(A2,2);//PORTC |= (1 << PC1); // PULLUP
+  pinMode(A3,2);
+  DDRB|= 1<<pin[3] | 1<<pin[2] | 1<<pin[1] | 1<<pin[0];
+
+  PCICR = 1<<1;
+  PCMSK1 = 1<<1 | 1<<2 | 1<<3;
+
+  if(DEBUG)Serial.begin(9600);
+}
+
+ISR(PCINT1_vect){
+  int a = PINC>>PC1 & 1;
+  int b = PINC>>PC2 & 1;
+  int c = PINC>>PC3 & 1;
+
+  if(a!=a0){  
+    a0 = a;
+    if(a==0) changeVal(0);
+  }
+  if(b!=b0){
+    b0 = b;
+    if(b==0) off();
+  }
+  if(c!=c0){
+    c0 = c;
+    if(c==0) changeVal(1);
+  }
+}
+
+void off(){
+  counter = 0;
+  PORTB=~(counter<<2);
+}
+
+void changeVal(bool up){
+  //binary();
+  counter = max(min(counter + (up ? 1 : -1), 15), 0);
+  PORTB=~(counter<<2);
+  if(DEBUG)debug();
+
+  //binar[0]==0 ? PORTB|=(1<<pin[0]):PORTB&=~(1<<pin[0]); //? digitalWrite(pin[0]+8,1):digitalWrite(pin[0]+8,0)
+}
+
+void debug(){
+  Serial.print("0b");
+  Serial.print(counter,2);
+  Serial.print("\t");
+  Serial.println(counter);
+}
+
+/*void binary(){
+  int dec = counter;
+  for(int i = 3;i>=0;i--){
+    binar[i]=dec%2;
+    dec = dec/2;
+  }
+}*/
+void loop(){}
